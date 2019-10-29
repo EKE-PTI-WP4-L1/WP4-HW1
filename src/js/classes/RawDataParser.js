6166f8044e636b5a8cf00e26cf9932e2ba9a7395
Szobonya Gergő <djoke23@freemail.hu>
2019-10-29 23:19:40 +0100

    RawDataParser osztály refaktorizálása.

--- src/js/classes/RawDataParser.js
+++ src/js/classes/RawDataParser.js
@@ -8,34 +8,51 @@ export default class RawDataParser {
         return rawArticle.split('$$$');
     }
 
-    static parseRawSuggestedLabels(rawSuggestedLabels) {
-        let rawLabels = rawSuggestedLabels.split('__label__');
+    static parseRawLabels(rawData, type) {
+        let rawLabels = RawDataParser.splitRawLabelsByType(rawData, type);
         let labels = RawDataParser.generateLabelsArray();
+
         rawLabels.forEach(function (rawLabel) {
             if (rawLabel !== '') {
-                let label = [];
-                let values = rawLabel.split(' ');
-                label['num'] = parseFloat(values[1]);
-                label['value'] = values[0].replace(/@@/g, ' ');
+                let label = RawDataParser.populateLabelValueByType(rawLabel, type);
                 RawDataParser.populateNormalAndCustomLabels(label, labels);
             }
         });
         return labels;
     }
 
-    static parseRawOriginalLabels(rawDefaultLabels) {
-        let rawLabels = rawDefaultLabels.split(' ');
-        let labels = RawDataParser.generateLabelsArray();
-        rawLabels.forEach(function (rawLabel) {
-            if (rawLabel !== '') {
-                let label = [];
-                label['value'] = rawLabel.replace(/@@/g, ' ');
-                label['value'] = label['value'].replace(/__label__/g, '');
-                RawDataParser.populateNormalAndCustomLabels(label, labels);
-            }
-        });
+    static splitRawLabelsByType(rawData, type) {
+        if (type === 'suggested') {
+            return rawData.split('__label__');
+        } else if (type === 'default') {
+            return rawData.split(' ');
+        } else {
+            throw 'Label type must be suggested or default.';
+        }
+    }
 
-        return labels;
+    static populateLabelValueByType(rawLabel, type) {
+        let label = [];
+        if (type === 'suggested') {
+            RawDataParser.populateSuggestedLabelValue(rawLabel, label)
+        } else if (type === 'default') {
+            RawDataParser.populateDefaultLabelValue(rawLabel, label)
+        } else {
+            throw 'Label type must be suggested or default.';
+        }
+        return label;
+    }
+
+    static populateSuggestedLabelValue(rawLabel, label) {
+        let values = rawLabel.split(' ');
+        label['num'] = parseFloat(values[1]);
+        let regexMap = {'@@': ' ', '__': ': '};
+        label['value'] = RawDataParser.replaceAllRegex(values[0], regexMap);
+    }
+
+    static populateDefaultLabelValue(rawLabel, label) {
+        let regexMap = {'@@': ' ', '__label__': '', '__': ': '};
+        label['value'] = RawDataParser.replaceAllRegex(rawLabel, regexMap);
     }
 
     static generateLabelsArray() {
@@ -46,10 +63,18 @@ export default class RawDataParser {
     }
 
     static populateNormalAndCustomLabels(label, labels) {
-        if (label['value'].match(/(geography__|organization__|person__ )/)) {
+        if (/(geography: |organization: |person: )/.test(label['value'])) {
             labels['custom'].push(label);
         } else {
             labels['normal'].push(label);
         }
+    }
+
+    static replaceAllRegex(str, regexMap) {
+        let re = new RegExp(Object.keys(regexMap).join('|'), 'gi');
+
+        return str.replace(re, function (matched) {
+            return regexMap[matched.toLowerCase()];
+        });
     }
 }
